c73ddf4ad8ecb8bc61c21fa2615e2f604031d82f
samu-koi305 <167172059+samu-koi305@users.noreply.github.com>
2024-04-19 09:30:27 +0900

    kadai_018を再アップロード

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -6,10 +6,10 @@ abstract public class Kato_Chapter18 {
 	String address;
 
 	public void commonIntroduce() {
-		familyName = "加藤";
-		address = "東京都中野区〇×";
-		System.out.println("名前は" + familyName + givenName + "です");
-		System.out.println("住所は" +address + "です");
+		this.familyName = "加藤";
+		this.address = "東京都中野区〇×";
+		System.out.println("名前は" + this.familyName + this.givenName + "です");
+		System.out.println("住所は" + this.address + "です");
 	}
 	
 	abstract void eachIntroduce();
